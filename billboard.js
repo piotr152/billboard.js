44d77c8394b60949a02ecbc1c82872f010d58306
piotr152 <piotr152@web.de>
2015-03-31 13:37:59 +0200

    Implemented a rudimentary REST API

--- billboard.js
+++ billboard.js
@@ -6,6 +6,7 @@ var io = require("socket.io")(http)
 var fs = require("fs")
 var swig = require("swig")
 var config = require("./modules/config")
+var bodyparser = require("body-parser")
 
 var args = require("optimist").argv
 
@@ -20,6 +21,9 @@ if (args.writelink) config.setWriteLink(args.writelink)
 console.log(config.css)
 
 app.use(express.static(path.join(__dirname, 'static')))
+app.use(bodyparser.urlencoded({
+        extended: true
+    }));
 app.engine('html', swig.renderFile)
 app.set("views", "./templates")
 app.set("view engine", "html")
@@ -28,8 +32,6 @@ require("./modules/router").setup(app)
 
 require("./modules/socket").setup(io)
 
-
-
-
+require("./modules/post").setup(io)
 
 http.listen(config.port, "localhost", function(){console.log("listening to port " + config.port)})
--- modules/socket.js
+++ modules/socket.js
@@ -1,21 +1,29 @@
 var fs = require("fs")
 var config = require("./config")
 
+socket = null
+
+exports.sendmessage = function(msg) {
+	console.log(msg)
+	socket.emit("chat message", msg)
+	fs.appendFile(config.log, JSON.stringify(msg) + "\n")
+}
+
+exports.deletelog = function() {
+	fs.unlink(config.log)
+	console.log("log deleted\n")
+	socket.emit("logdeleted")
+}
+
 exports.setup = function(io){
+	socket = io
 	io.on("connection", function(socket) {
 	console.log("a user connected")
 	socket.on("disconnect", function() {
 		console.log("user disconnected")
 	})
 
-	socket.on("chat message", function(msg) {
-		console.log(msg)
-		io.emit("chat message", msg)
-		fs.appendFile(config.log, JSON.stringify(msg) + "\n")
-	})
+	socket.on("chat message", exports.sendmessage)
 
-	socket.on("deletelog", function(msg) {
-		fs.unlink(config.log)
-		io.emit("logdeleted")
-	})
+	socket.on("deletelog", exports.deletelog)
 })}
